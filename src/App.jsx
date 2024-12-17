97c3a12680ead73e72e8b815e678d7e8818f6e01
ArmenSyan <armen.sahakyan.ait@gmail.com>
2024-12-17 18:20:22 +0400

    Create Todo, Testing, Done jsx files and add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Todo from './Components/Todo'
+import Testing from './Components/Testing'
+import Done from './Components/Done'
 
 function App() {
 
   return (
     <>
+    <Todo />
+    <Testing />
+    <Done />
     </>
   )
 }
